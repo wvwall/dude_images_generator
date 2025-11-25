c41095090b08cc0a00cd27b28451372bf6733d33
Walter Velardo <waltervelardo@gmail.com>
2025-11-25 09:59:07 +0100

    replace header icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ImagePlus, Menu, X } from "lucide-react";
+import { Coffee, Menu, X } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
 const Header: React.FC = () => {
@@ -25,10 +25,10 @@ const Header: React.FC = () => {
     <header className="top-0 z-50 flex items-center justify-between w-full px-4 py-4 bg-white border-b-4 shadow-sm md:px-24 border-friends-purple">
       <div className="flex items-center gap-4">
         <div className="flex items-center justify-center w-12 h-12 border-2 rounded-full shadow-lg bg-friends-purple border-friends-yellow">
-          <ImagePlus size={24} className="text-white" />
+          <Coffee size={24} className="text-white" />
         </div>
         <div className="flex flex-col">
-          <h1 className="flex items-center gap-1 text-3xl tracking-wider text-black font-hand">
+          <h1 className="flex items-center gap-1 text-4xl tracking-wider text-black font-hand">
             D <span className="w-2 h-2 mt-2 rounded-full bg-friends-red"></span>
             U<span className="w-2 h-2 mt-2 rounded-full bg-friends-blue"></span>
             D
